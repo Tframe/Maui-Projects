5a10f8bc36bbe3c3d7383f9ac7ddff8e0763bb07
Trevor Frame <tframe@esri.com>
2023-04-05 10:35:57 -0700

    Add Carousel View Media Element Repro
    Add Carousel View Media Element Repro

---
+++ CarouselMediaSample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace CarouselMediaSample;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    void mediaElement_HandlerChanging(System.Object sender, Microsoft.Maui.Controls.HandlerChangingEventArgs e)
+    {
+        Console.WriteLine($"Changing handler from {e.OldHandler} to {e.NewHandler}");
+    }
+
+    void mediaElement_StateChanged(System.Object sender, CommunityToolkit.Maui.Core.Primitives.MediaStateChangedEventArgs e)
+    {
+        Console.WriteLine($"Changing State from {e.PreviousState} to {e.NewState}");
+    }
+
+    void mediaElement_ParentChanging(System.Object sender, Microsoft.Maui.Controls.ParentChangingEventArgs e)
+    {
+        Console.WriteLine($"Changing parent from {e.OldParent} to {e.NewParent}");
+    }
+}
+
+
---
+++ CarouselMediaSample/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("CarouselMediaSample.MainApplication, CarouselMediaSample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6440e486131de8c9e4.MainApplication.class, crc6440e486131de8c9e4.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
